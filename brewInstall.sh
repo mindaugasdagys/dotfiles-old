a3db26500ae6109e567f642c42aba51335ed06fa
Mindaugas Dagys <admin@mindaugasdagys.com>
2015-01-09 12:51:29 +0200

    Add GNU coreutils

--- brewInstall.sh
+++ brewInstall.sh
@@ -1,6 +1,24 @@
-# Installs Homebrew packages
+# This script installs Homebrew packages
+
+# Update Homebrew
 brew update
 brew upgrade
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# Install Bash 4
+brew install bash
+
+# Install more recent versions of some OS X tools
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+
+# update the $PATH in your ~/.bash_profile in order to use these tools over their Mac counterparts
+$PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
 
 brew install zsh
 
