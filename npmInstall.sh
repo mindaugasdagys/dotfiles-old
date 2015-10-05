53574efcefb0b6c93bb03ae79adbab88fa79224f
Mindaugas Dagys <admin@mindaugasdagys.com>
2015-10-05 14:28:56 +0300

    add babel, web pack

--- npmInstall.sh
+++ npmInstall.sh
@@ -1,16 +1,6 @@
 # Node packages
 
-npm install -g bower
-npm install -g grunt
-npm install -g gulp
-npm install -g yo
+#npm install -g yo
 
-npm install -g generator-angular
-npm install -g generator-ionic
-
-npm install -g brunch
-
-npm install -g cordova
-npm install -g phonegap
-npm install -g ionic
-npm install -g ios-sim
+npm install -g babel
+npm install -g webpack
