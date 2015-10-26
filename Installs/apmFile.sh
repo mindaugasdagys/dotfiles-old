7388f148d8d5c10eb5f695f5ccef0f3f6728770f
Mindaugas Dagys <admin@mindaugasdagys.com>
2015-10-26 14:09:49 +0200

    add Atom packages install script

---
+++ Installs/apmFile.sh
@@ -0,0 +1,17 @@
+# Atom editor user packages
+
+# Package helps download predefined and custom list of packages. This is useful in two cases:
+# - If you don't want to look around for recommended packages for your programming language
+# - If you want to share your own package list with your friend or with the world :).
+#   Right now you can do it using Gist
+apm install package-list-downloader
+
+apm install emmet
+
+apm install linter
+#apm install javascript-snippets
+
+# Runs CoffeeScript or JavaScript in Atom
+apm install run-in-atom
+
+apm install
