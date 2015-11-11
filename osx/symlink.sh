afd81c5d47750f9642f93770e034d9e634648a82
Mindaugas Dagys <admin@mindaugasdagys.com>
2015-11-11 13:46:53 +0200

    add some video symlinks

--- osx/symlink.sh
+++ osx/symlink.sh
@@ -3,6 +3,12 @@
 # MOVIES
 ln -s /Volumes/Diskas\ 2TB/\!Video ~/Movies/
 
+# VIDEOS
+ln -s /Volumes/Diskas\ 2TB/Videos/VideoMac ~/Movies
+ln -s /Volumes/Diskas\ 2TB/Videos/VideoMac/JavaScript\ training\ videos ~/Movies
+ln -s /Volumes/Diskas\ 2TB/Videos/VideoMac/JavaScript\ training\ videos/ES6\ Video ~/Movies
+ln -s /Volumes/Diskas\ 2TB/Videos/VideoMac/JavaScript\ training\ videos/ReactJS ~/Movies
+
 # MUSIC
 ln -s /Volumes/Diskas\ 2TB/Music ~/Music/
 
